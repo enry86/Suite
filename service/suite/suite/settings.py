ce364399a7eee59b8b7e507bdb349b142fdc3192
Enrico Sartori <sartori.enrico@gmail.com>
2014-07-18 11:25:47 +0200

    added missing files

--- service/suite/suite/settings.py
+++ service/suite/suite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -23,6 +22,7 @@ SECRET_KEY = 'oj-23*8+%26c11@k-w@m$g3&@9&i4z2-+w(wao0!lpybi8p%$a'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+TEMPLATE_DIRS = [os.path.join (BASE_DIR, 'templates')]
 
 ALLOWED_HOSTS = []
 
--- service/suite/suite_core/models.py
+++ service/suite/suite_core/models.py
@@ -23,6 +23,9 @@ class PriceList (models.Model):
     date_end = models.DateField ()
     price = models.DecimalField (max_digits = 10, decimal_places = 2)
     service = models.ForeignKey (Service)
+    
+    def __str__ (self):
+        return self.name
 
 #Modifiers list
 class Modifier (models.Model):
